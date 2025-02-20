b9876db6f467e4b1310ac1513f2b7e6dc0f2ad40
Gennita <genny.laguado@gmail.com>
2025-02-19 20:38:20 -0500

    cambios 19 feb

--- Aprendizaje_Javascript/index.js
+++ Aprendizaje_Javascript/index.js
@@ -1,10 +1,9 @@
-let numero = 3;
-let modulo = numero % 2;
-console.log(numero);
-if (modulo == 0) {
-  console.log("Es par");
-}
+let saldo = 5000
 
-if (modulo != 0) {
-  console.log("Es impar");
+if (saldo > 1000) {
+  console.log("Saldo suficiente")
+} else if (saldo>500) {
+    console.log("Saldo insuficiente")
+} else {
+    console.log("Saldo crítico")
 }
